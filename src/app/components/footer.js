599145bc71c9c9c01b7e84f5f617c152fdee5e1d
Ajith <ajithsa900@gmai.com>
2024-09-03 22:59:26 +0530

    footer edited2

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="text-center">
           <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-            <Image src="/Atom-logo-2.png" className="h-6 mr-3 sm:h-9" alt="Landwind Logo" width={120} height={120} />
+            <Image src="/Atom-logo-2.png" className="h-6 mr-3 sm:h-9 " alt="Landwind Logo" width={200} height={460} />
             </a>
           <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
             © 2024-2025 AToM IAS™. All Rights Reserved. Built with 
