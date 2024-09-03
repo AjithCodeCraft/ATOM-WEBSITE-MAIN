56abf88c1a8b455eb20713cf89d12921df6b8f91
Ajith <ajithsa900@gmai.com>
2024-09-03 22:49:14 +0530

    footer edited

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -8,34 +8,11 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="text-center">
           <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-            <Image src="/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Landwind Logo" width={36} height={36} />
-            Landwind
-          </a>
+            <Image src="/Atom-logo-2.png" className="h-6 mr-3 sm:h-9" alt="Landwind Logo" width={120} height={120} />
+            </a>
           <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
-            © 2021-2022 Landwind™. All Rights Reserved. Built with <a href="https://flowbite.com" className="text-purple-600 hover:underline dark:text-purple-500">Flowbite</a> and <a href="https://tailwindcss.com" className="text-purple-600 hover:underline dark:text-purple-500">Tailwind CSS</a>.
+            © 2024-2025 AToM IAS™. All Rights Reserved. Built with 
           </span>
-          <ul className="flex justify-center mt-5 space-x-5">
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-                {/* Add your SVG icon here */}
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-                {/* Add your SVG icon here */}
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-                {/* Add your SVG icon here */}
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-                {/* Add your SVG icon here */}
-              </a>
-            </li>
-          </ul>
         </div>
       </div>
     </footer>
