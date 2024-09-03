66e3042fe6d395162843ddefaca3023b11e7bebf
vivekr1998 <viveknimz98@gmail.com>
2024-09-03 21:55:02 +0530

    course and footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,102 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 
-const Footer = () => {
+const footer = () => {
   return (
     <footer className="bg-white">
       <div className="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
-          <div>
-            <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">About</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Careers</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Brand Center</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Blog</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help center</h3>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Discord Server</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Twitter</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Facebook</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h3>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Privacy Policy</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Licensing</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Terms</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">About</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Careers</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Brand Center</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Blog</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h3>
-            <ul className="text-gray-500 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">iOS</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Android</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">Windows</a>
-              </li>
-              <li className="mb-4">
-                <a href="#" className="hover:underline">MacOS</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="text-center">
-          <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-            <Image src="/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Landwind Logo" width={36} height={36} />
-            Landwind
+          <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-black">
+            <Image src="/Atom-logo-2.png" className="h-9 mr-9 sm:h-9" width={70} height={70}/> ATOM IAS
           </a>
-          <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
-            © 2021-2022 Landwind™. All Rights Reserved. Built with <a href="https://flowbite.com" className="text-purple-600 hover:underline dark:text-purple-500">Flowbite</a> and <a href="https://tailwindcss.com" className="text-purple-600 hover:underline dark:text-purple-500">Tailwind CSS</a>.
+          <span className="block text-sm text-center text-black">
+            © 2021-2022 ATOM IAS™. All Rights Reserved. 
           </span>
           <ul className="flex justify-center mt-5 space-x-5">
             <li>
@@ -126,4 +42,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+export default footer;
