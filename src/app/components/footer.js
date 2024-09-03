dcc5c893007f2a26e10556b88e5e43476b283aef
Ajay M V <ajaymvaju17@gmail.com>
2024-09-03 23:35:18 +0530

    functions

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const Footer = () => {
   return (
     <footer className="bg-white">
-      <div className="max-w-screen-xl p-4 py-6 mx-auto lg:py-8 md:p-8 lg:p-10">
+      <div className="max-w-screen-xl p-4 mx-auto md:p-8 lg:p-10">
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="text-center">
           <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-            <Image src="/Atom-logo-2.png" className="h-6 mr-3 sm:h-9 " alt="Landwind Logo" width={200} height={460} />
+            <Image src="/Atom-logo-2.png" className="object-cover" alt="Landwind Logo" width={160} height={170} />
             </a>
           <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
             © 2024-2025 AToM IAS™. All Rights Reserved. Built with 
--- src/app/page.js
+++ src/app/page.js
@@ -21,50 +21,52 @@ export default function Home() {
         <meta name="description" content={metadata.description} />
         <title>{metadata.title}</title>
       </Head>
-      <Header />
-      <section className="bg-white lg:pt-10 max-w-screen-xl px-6 lg:px-12 pt-10 pb-3 mx-auto">
-        <div className="lg:grid lg:grid-cols-12 lg:py-14 lg:pt-18">
+      <body className='flex flex-col gap-0'>
+        <Header />
+        <section className="bg-white lg:pt-10 max-w-screen-xl px-6 lg:px-12 pt-10 pb-3 mx-auto">
+          <div className="lg:grid lg:grid-cols-12 lg:py-14 lg:pt-18">
 
-          <div className="lg:col-span-7 flex flex-col justify-center items-center lg:items-start text-center lg:text-left lg:pr-8">
-            <h1 className="text-indigo-600 dark:text-indigo-400 text-2xl font-bold md:text-3xl lg:text-4xl xl:text-5xl">
-              Welcome to Atom
-            </h1>
-            <h2 className="max-w-2xl mb-4 text-3xl font-bold tracking-tight md:text-4xl xl:text-4xl text-black dark:text-black">
-              The Cosmic Hub for IAS Aspirants
-            </h2>
+            <div className="lg:col-span-7 flex flex-col justify-center items-center lg:items-start text-center lg:text-left lg:pr-8">
+              <h1 className="text-indigo-600 dark:text-indigo-400 text-2xl font-bold md:text-3xl lg:text-4xl xl:text-5xl">
+                Welcome to Atom
+              </h1>
+              <h2 className="max-w-2xl mb-4 text-3xl font-bold tracking-tight md:text-4xl xl:text-4xl text-black dark:text-black">
+                The Cosmic Hub for IAS Aspirants
+              </h2>
 
-            <p className="max-w-2xl mb-4 text-black dark:text-gray-300 text-base font-normal lg:text-lg">
-              We&apos;re not your average study buddies. We&apos;re stardust-infused mentors, caffeine-fueled dream weavers, and syllabus navigators.
-            </p>
+              <p className="max-w-2xl mb-4 text-black dark:text-gray-300 text-base font-normal lg:text-lg">
+                We&apos;re not your average study buddies. We&apos;re stardust-infused mentors, caffeine-fueled dream weavers, and syllabus navigators.
+              </p>
 
-            <div className="flex flex-row justify-center lg:justify-start gap-3 mt-4">
-              <button className="bg-[#4f46e5] text-white text-xs md:text-base rounded-full py-2 px-4 md:px-8 transition-transform duration-300 border border-[#4f46e5] hover:scale-105 hover:shadow-inner">
-                Start for free
-              </button>
-              <button className="bg-white text-[#4f46e5] text-xs md:text-base rounded-full py-2 px-4 md:px-8 transition-transform duration-300 border border-[#4f46e5] hover:scale-105 hover:bg-[#4f46e5] hover:text-white hover:shadow-inner">
-                Connect with us
-              </button>
+              <div className="flex flex-row justify-center lg:justify-start gap-3 mt-4">
+                <button className="bg-[#4f46e5] text-white text-xs md:text-base rounded-full py-2 px-4 md:px-8 transition-transform duration-300 border border-[#4f46e5] hover:scale-105 hover:shadow-inner">
+                  Start for free
+                </button>
+                <button className="bg-white text-[#4f46e5] text-xs md:text-base rounded-full py-2 px-4 md:px-8 transition-transform duration-300 border border-[#4f46e5] hover:scale-105 hover:bg-[#4f46e5] hover:text-white hover:shadow-inner">
+                  Connect with us
+                </button>
+              </div>
+            </div>
+
+
+            <div className="lg:col-span-5 flex justify-center items-center mt-6 lg:mt-4 lg:pl-8">
+              <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+                <Image src="/hero-img.png" alt="Hero image" layout="responsive" width={300} height={300} className="rounded-lg object-cover" />
+              </div>
             </div>
           </div>
-
-
-          <div className="lg:col-span-5 flex justify-center items-center mt-6 lg:mt-4 lg:pl-8">
-            <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
-              <Image src="/hero-img.png" alt="Hero image" layout="responsive" width={300} height={300} className="rounded-lg object-cover" />
-            </div>
+          <div className="h-8 justify-start items-center mx-auto gap-4 inline-flex flex-wrap">
+            <Link href='#courses' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal md:pr-6 pr-2 border-r-[1px] border-stone-300 hover:underline">Mains 2025</Link>
+            <Link href='#courses' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal md:pr-6 pr-2 border-r-[1px] border-stone-300 hover:underline">Mains 2024</Link>
+            <Link href='#courses' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal md:pr-6 pr-2 border-r-[1px] border-stone-300 hover:underline">Companionship</Link>
+            <Link href='#' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal hover:underline">Question Booklet Creator</Link>
           </div>
-        </div>
-        <div className="h-8 justify-start items-center mx-auto gap-4 inline-flex flex-wrap">
-          <Link href='#courses' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal md:pr-6 pr-2 border-r-[1px] border-stone-300 hover:underline">Mains 2025</Link>
-          <Link href='#courses' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal md:pr-6 pr-2 border-r-[1px] border-stone-300 hover:underline">Mains 2024</Link>
-          <Link href='#courses' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal md:pr-6 pr-2 border-r-[1px] border-stone-300 hover:underline">Companionship</Link>
-          <Link href='#' className="text-black dark:text-white md:text-lg text-xs font-normal font-['Inter'] leading-normal hover:underline">Question Booklet Creator</Link>
-        </div>
-      </section>
-      <WhyChoose />
-      <Course />
-      <Contact />
-      <Footer />
+        </section>
+        <WhyChoose />
+        <Course />
+        <Contact />
+        <Footer />
+      </body>
     </>
   );
 }
